da1da7db2853aa0bc0f2892b88e2671ba04f74b4
Gene Sluder <gene@gobiko.com>
2020-03-05 08:28:54 -0800

    Create order only on ticket add

--- agiletix/cart.py
+++ agiletix/cart.py
@@ -23,84 +23,49 @@ api = AgileSalesAPI(
     corp_org_id=SETTINGS['AGILE_CORP_ORG_ID']
 )
 
-
-def get_cart_for_request(request):
-    """
-    Try to retrieve cart from the current session.
-
-    """
-    if hasattr(request, 'cart'):
-        cart = request.cart
-    else:
-        cart = Cart(request)
-    if cart:
-        if cart.is_valid:
-            request.cart = cart # Store cart on the request for the next call
-            return cart
-        else:
-            request.session[SESSION_CART_DATA] = None
-
-    return None
-
-
-def get_or_create_cart_for_request(request, force_non_member=False):
+def get_cart_for_request(request, force_non_member=False):
     """
     Try to retrieve cart from the current session. If none found, create one
 
     """
     cart = None
-    try:
-        cart = get_cart_for_request(request)
-        if not cart or (force_non_member and cart and cart.is_member):
-            cart = Cart(request)
-            cart.start_order(force_non_member=force_non_member)
-    except AgileException as e:
-        # TODO: Yeha
-        #logger.warning(__name__, "AgileException -> {}".format(e))
-        if e.code == 1024:
-            cart = get_or_create_cart_for_request(request, force_non_member=True)
-        #cart_error = e.code
-
+    if hasattr(request, 'cart'):
+        cart = request.cart
+    if cart and cart.is_member and force_non_member:
+        cart = None
+    if not cart:
+        try:
+            cart = Cart(request=request, force_non_member=force_non_member) 
+        except AgileException as e:
+            # TODO: Yeha
+            #logger.warning(__name__, "AgileException -> {}".format(e))
+            if e.code == 1024:
+                cart = get_cart_for_request(request, force_non_member=True)
+            #cart_error = e.code
     return cart
 
 
-    def get_cart(self, request):
-        cart_error = None
-        cart = None
-
-        if not cart:
-            logger.warning(__name__, "Couldn't load cart")
-            cart_error = -1
-        return cart, cart_error
-
-
-
-
 class Cart(object):
+    _order = None
+    customer = None
+    request = None
 
-    def __init__(self, request, order=None):
+    def __init__(self, request, force_non_member=False, order=None):
         self._order = None
         self.request = request
 
-        self.is_member = False
-
-        if request.user.is_authenticated:
-            customer = request.user
-            if customer.member_id:
-                self.is_member = True
+        if request.user.is_authenticated and not force_non_member:
+            self.customer = request.user
 
         if order:
             self.order = order
 
-    def start_order(self, force_non_member=False):
-        customer = None
+    def start_order(self):
+        customer = self.customer
         response = None    
 
-        if self.request.user.is_authenticated:
-            customer = self.request.user
-
         if customer:
-            if customer.member_id and not force_non_member:
+            if customer.member_id:
                 response = api.order_start(buyer_type_id=SETTINGS['AGILE_BUYER_TYPE_STANDARD_ID'] , customer_id=customer.customer_id, member_id=customer.member_id)
             else:
                 response = api.order_start(buyer_type_id=SETTINGS['AGILE_BUYER_TYPE_STANDARD_ID'] , customer_id=customer.customer_id)
@@ -121,10 +86,10 @@ class Cart(object):
 
         self.order = order
 
-    def load_order(self, request):
+    def load_order(self):
         order = None
         json_object = None
-        order_json = request.session.get(SESSION_CART_DATA)
+        order_json = self.request.session.get(SESSION_CART_DATA)
         if order_json:
             try:
                 json_object = json.loads(order_json)
@@ -146,49 +111,36 @@ class Cart(object):
         
         return order 
 
+    def validate_order(self, order):
+        valid = order.in_process
+        if order.expiration_datetime < timezone.now() or order.expired:
+            valid = False
+
+        customer = self.customer
+        if customer and customer.customer_id:
+            if not order.customer_id:
+                valid = False
+            elif (int(order.customer_id) != int(customer.customer_id)):
+                valid = False
+        return valid
+
     @property
     def order(self):
         if not self._order:
-            self._order = self.load_order(self.request)
+            order = self.load_order()
+            if order:
+                if not self.validate_order(order):
+                    self.request.session[SESSION_CART_DATA] = None
+                    order = None
+            if not order:
+                order = self.start_order()
+            self._order = order
         return self._order
 
     @order.setter
     def order(self, value):
         self._order = None
         self.request.session[SESSION_CART_DATA] = json.dumps(value.to_json())
-
-    @property
-    def is_valid(self):
-        if not hasattr(self, '_is_valid'):
-            self._is_valid = (self.order is not None and not self.is_expired and self.is_in_process and self.cart_customer_matches_current)
-        return self._is_valid
-
-    @property
-    def is_in_process(self):
-        in_process = False
-        response = api.order_status(self.order.order_id, self.order.transaction_id)
-        if response and response.success:
-            try:
-                order = Order(response.data)
-                in_process = order.in_process
-            except:
-                # TODO: Better handling
-                pass
-        return in_process
-
-    @property
-    def is_expired(self):
-        expired = (self.order.expiration_datetime < timezone.now()) or self.order.expired
-        return expired
-
-    @property
-    def cart_customer_matches_current(self):
-        customer = None
-        if self.request.user.is_authenticated:
-            customer = self.request.user
-        if customer and customer.customer_id and self.order.customer_id and (int(self.order.customer_id) != int(customer.customer_id)):
-            return False
-        return True
 
     def add_tickets(self, agile_event_org_id, agile_event_id, tier_id, tickets, promo_codes=None):
         """
@@ -244,6 +196,7 @@ class Cart(object):
     def get_transfer_url(self):
         response = api.order_transfer(self.order.order_id, self.order.transaction_id)
         url = None
+        logger.debug("Transfer URL response", response=response.data) 
         if response.success:
             url = response.data
             url = url.replace('http://', 'https://')
